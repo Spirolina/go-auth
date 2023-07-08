31e7fb1045692f5ed1c5ce3e9a429e23c2363d1e
Güner Arda Işık <g.isik.arda@gmail.com>
2023-07-08 14:21:48 +0300

    read template from file

--- handlers/default_handler.go
+++ handlers/default_handler.go
@@ -2,15 +2,43 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func Welcome(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusAccepted)
-	fmt.Fprintf(w, "hello World")
-}
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
-func Dashboard(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusAccepted)
-	fmt.Fprint(w, "Hello from dashboard")
+	dat, err := os.ReadFile("./static/html/index.html")
+
+	tpl := string(dat)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	check := func(err error) {
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	t, err := template.New("webpage").Parse(tpl)
+	check(err)
+
+	data := struct {
+		Title string
+		Items []string
+	}{
+		Title: "My page",
+		Items: []string{
+			"My photos",
+			"My blog",
+		},
+	}
+	err = t.Execute(w, data)
+	check(err)
+
 }
