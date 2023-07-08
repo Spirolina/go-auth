e8149ec92353eeb54aa0d555e4c1b9973ada2170
Güner Arda Işık <g.isik.arda@gmail.com>
2023-07-08 13:59:27 +0300

    add template

--- main.go
+++ main.go
@@ -1,12 +1,60 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 
-	"github.com/Spirolina/go-auth/handlers"
+	"github.com/julienschmidt/httprouter"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	const tpl = `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>{{.Title}}</title>
+		</head>
+		<body>
+			{{range .Items}}<div>{{ . }}</div>{{else}}<div><strong>no rows</strong></div>{{end}}
+		</body>
+	</html>`
+
+	check := func(err error) {
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	t, err := template.New("webpage").Parse(tpl)
+	check(err)
+
+	data := struct {
+		Title string
+		Items []string
+	}{
+		Title: "My page",
+		Items: []string{
+			"My photos",
+			"My blog",
+		},
+	}
+
+	// noItems := struct {
+	// 	Title string
+	// 	Items []string
+	// }{
+	// 	Title: "My another page",
+	// 	Items: []string{},
+	// }
+
+	err = t.Execute(w, data)
+	check(err)
+
+}
+
 func main() {
-	http.HandleFunc("/", handlers.Welcome)
-	http.ListenAndServe(":5500", nil)
+	router := httprouter.New()
+	router.GET("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
